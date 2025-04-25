14a864c946015ed49f5d7a6012f0768e012c37dd
pepicont <stefano.conti2004@gmail.com>
2025-04-25 18:47:16 -0300

    pasé el 2

--- ejercicios.js
+++ ejercicios.js
@@ -3,3 +3,22 @@ let miArray = [1,2,3,4,5,6,7,8,9,10];
 for (let i=0;i<miArray.length;i++){
   console.log(miArray[9-i]);
 }*/
+
+/*EJERCICIO 2
+const funcEj2 = (arrayPalabras, cadenaString) => {
+    let cont = 0;
+   
+    // Recorrer el array con forEach
+    arrayPalabras.forEach(element => {
+        if (element.includes(cadenaString)) {
+            cont++;
+        }
+    });
+   
+    // Imprimir el resultado
+    console.log(`La palabra "${cadenaString}" se encuentra ${cont} veces.`);
+}
+
+const palabras = [`hola`, `holanda`, `ada`,`holagavria`];
+funcEj2(palabras, `hola`);  // Salida: La palabra "hola" se encuentra 2 veces.
+*/
