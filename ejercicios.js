45b06c27624ac8407d44dde9646af591db66d5f9
pepicont <stefano.conti2004@gmail.com>
2025-04-26 11:08:41 -0300

    Hice un algoritmo copado para Cx y lo dejo acá

--- ejercicios.js
+++ ejercicios.js
@@ -22,3 +22,18 @@ const funcEj2 = (arrayPalabras, cadenaString) => {
 const palabras = [`hola`, `holanda`, `ada`,`holagavria`];
 funcEj2(palabras, `hola`);  // Salida: La palabra "hola" se encuentra 2 veces.
 */
+
+let cadena= `abaaacbdeiiioeeaaabbcduaabbbbbbcbecdabdddioeuaaaiiiibebeeecdbdddciiioeuee`;
+let letra=[`a`,`b`,`c`,`d`,`e`,`i`,`u`,`o`];
+let cont2=0;
+console.log(`la cantidad de símbolos que hay son ${cadena.length}`);
+for(let j=0;letra.length>j;j++){
+    let cont=0;
+    for (let i=0;i<cadena.length;i++){
+        if(letra[j]==cadena[i])
+            cont++;
+}
+cont2+=cont;
+console.log(`la letra ${letra[j]} se encuentra ${cont} veces`);
+}
+console.log(`verificación: ${cont2}`);
